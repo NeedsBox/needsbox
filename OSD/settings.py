e57241fcf60ed0b867fd294632e58d7204d465cc
scutelniciuc <scutelniciuc.dev@gmail.com>
2020-12-21 22:41:25 +0000

    Added ALL ALLOWED_HOSTs

--- OSD/settings.py
+++ OSD/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = ['needs-box-9d8vt.ondigitalocean.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
