9e019fb1fba78dca126d2d81885abb726b65609f
scutelniciuc <scutelniciuc.dev@gmail.com>
2021-01-10 21:49:06 +0000

    Added library

--- project/admin.py
+++ project/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Advertisement, Review
+from .models import Category, Advertisement, Review, Location
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Advertisement)
 admin.site.register(Review)
+admin.site.register(Location)
