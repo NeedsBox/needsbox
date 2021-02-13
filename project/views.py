53ef5f257e84114a09f7238ae4d41190a85a6c3f
scutelniciuc <scutelniciuc.dev@gmail.com>
2021-02-13 02:47:36 +0000

    Added Q lookup

--- project/views.py
+++ project/views.py
@@ -1,7 +1,9 @@
+from django.db.models.lookups import IContains
 from spatialdata.models import Limits
 from django.shortcuts import render
 from .models import Category, Service
 from django.views.generic import ListView
+from django.db.models import Q, query
 # Create your views here.
 
 def index(request):
@@ -22,8 +24,10 @@ def search(request):
     print(request.GET["distrito"])
     print(request.GET["concelho"])
     
+    services = Service.objects.filter(Q(title__icontains=request.GET["search"]) | Q(user__username__icontains=request.GET["search"]))
+    
     categorias = Category.objects.all()
-    services = Service.objects.all()
+    #services = Service.objects.all()
     services_count = services.count()
 
     context = {
