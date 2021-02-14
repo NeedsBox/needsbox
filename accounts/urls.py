387c207606af225942adc5063e5a31c6e98a3c83
CaetanoSiik <jalbertocl2000@hotmail.com>
2021-02-14 16:44:03 +0000

    Change_password

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,9 @@ from django.contrib.auth import views as auth_views
 app_name = 'accounts'
 
 urlpatterns = [
+
     path('login/', auth_views.LoginView.as_view(authentication_form=forms.LoginForm), name='login'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import RegisterForm
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
@@ -10,7 +15,7 @@ class UserRegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('needsbox:index')
-    
+
     # Se o utilizador for criado com sucesso envio um email de agradecimento pelo registo
     def form_valid(self, form):
         """message = "Hello {name}, welcome to my Unsplash-Clone made in Django!\n\n Any questions contact me at unsplash@scutelniciuc.xyz\n\nBest regards,\nAlexandru".format(
@@ -22,3 +27,20 @@ class UserRegisterView(generic.CreateView):
             recipient_list=[form.cleaned_data.get('email')],
         )"""
         return super().form_valid(form)
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
