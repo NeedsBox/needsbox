97648e5e75a0ebdc330d68585616b1673c338edf
CaetanoSiik <jalbertocl2000@hotmail.com>
2021-02-14 21:24:10 +0000

    password-reset_email done

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,15 +9,23 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html', email_template_name='password_reset_email.html'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html',
+                                                                 email_template_name='password_reset_email.html',
+                                                                 success_url=reverse_lazy(
+                                                                     'accounts:password_reset_done')),
          name='password_reset'),
 
-    path('password-reset/done/',
+    path('password-reset-done',
          auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
 
     path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html',
+                                                     success_url=reverse_lazy('accounts:password_reset_complete')),
          name='password_reset_confirm'),
+
+    path('password-reset-complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete')
 
 ]
