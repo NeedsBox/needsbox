353f506d0aa919b93370dd7cb9f4955fae97ae08
CaetanoSiik <jalbertocl2000@hotmail.com>
2021-02-15 01:51:26 +0000

    change_password_email fix reverse_lazy

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from . import views, forms
 
 app_name = 'accounts'
